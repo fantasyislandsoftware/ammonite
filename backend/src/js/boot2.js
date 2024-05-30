436f2c946c9a37c0616b297dd08be3eb8c35fe4b
Andy Jones <>
2024-05-30 16:49:19 +0100

    Improved Jam execution and jumps

--- backend/src/js/boot2.js
+++ backend/src/js/boot2.js
@@ -2,9 +2,8 @@ import { LOGIC_API } from "LOGIC_API";
 import { SYSTEM_API } from "SYSTEM_API";
 import { SCREEN_API } from "SCREEN_API";
 
-console.log("start");
-
 label("test_label");
-jp("test_label");
+//console.log('hello');
+jpif("test_label", "strange".includes("range"), true);
 
 console.log("end");
--- frontend/src/functions/tasks.ts
+++ frontend/src/functions/tasks.ts
@@ -35,39 +35,63 @@ export enum IData {
   VAR = 'var',
 }
 
+const DO_NOT_PROCESS = '{do_not_process}';
+const BREAK = '{break}';
+
 const squash = (str: string) => {
   return str.toLowerCase().trimStart();
+};
+
+const pass1 = (data: string) => {
+  const result: string[] = [];
+  const lines = data.split('\n');
+  lines.forEach((line) => {
+    line = squash(line);
+    if (line.startsWith('//') || line === '') {
+      line = '{remove}';
+    }
+    if (line !== '{remove}') {
+      result.push(line);
+    }
+  });
+  return result.join('\n');
 };
 
 export const startTask = async (path: string) => {
   const name = path.substring(path.lastIndexOf('/') + 1);
   const { tasks, setTasks } = useTaskStore.getState();
   let data = await getFile(path, EnumDataFormat.TEXT);
-  data = data.split('\n');
+
+  data = pass1(data);
+
+  data = data
+    .replaceAll('\n', '')
+    .replaceAll(';', BREAK)
+    .replaceAll('*/', BREAK)
+    .split(BREAK);
+  //console.log(data);
   const lines: string[] = [];
   const label: any = {};
   data.forEach((line: string, index: number) => {
-    /* Imports */
-    if (squash(line).startsWith('import')) {
-      line = `// ${line}`;
+    /* Remove comment remains */
+    if (line.startsWith('/*')) {
+      line = DO_NOT_PROCESS;
     }
-    line = line.replaceAll('LOGIC_API', 'logic_api');
-    line = line.replaceAll('SYSTEM_API', 'system_api');
-    line = line.replaceAll('SCREEN_API', 'screen_api');
+
+    /* Remove Imports */
+    if (line.startsWith('import')) {
+      line = DO_NOT_PROCESS;
+    }
 
     /* Label */
-
-    if (squash(line).startsWith('label')) {
-      const l_a = squash(line).split('label("')[1];
-      const l_b = l_a.split('");')[0];
-      label[l_b] = index;
-      line = `// ${line}`;
+    if (line.startsWith('label')) {
+      const l = line.split('"')[1];
+      label[l] = index;
+      line = DO_NOT_PROCESS;
     }
 
-    /* Add */
     lines.push(line);
   });
-  console.log(lines);
   tasks.push({
     id: uuidv4(),
     name: name,
@@ -92,48 +116,26 @@ export const startTaskProcessor = () => {
   }, 1);
 };
 
-export const analyseCommand = (task: ITask) => {
-  const str = task.code[task.pos];
-  const splitA = str.split('(');
-  const funcName = splitA[0];
-  let rem = splitA[1];
-  if (rem.endsWith(');')) {
-    rem = rem.substring(0, rem.length - 2);
-  }
-  const params = rem.split(',');
-  const parray: IParam[] = [];
-  params.forEach((param) => {
-    const id = param.trim();
-    let type = IData.CONSTANT;
-    let value = id;
-
-    if (id in task.var) {
-      type = IData.VAR;
-      value = task.var[id];
-    }
-
-    //@ts-ignore
-    if (!isNaN(value)) {
-      //@ts-ignore
-      value = parseInt(value);
-    } else {
-      if (typeof value === 'string') {
-        value = value.replaceAll(`"`, '');
-      }
-    }
-
-    const processedParam: IParam = { id: id, value: value, type: type };
-    parray.push(processedParam);
-  });
-  return { func: funcName, params: parray };
-};
-
 export const execCommand = (self: ITask) => {
   const jp = (label: string) => {
     self.pos = self.label[label];
   };
 
-  const line = self.code[self.pos];
+  const jpif = (label: string, condition?: any, value?: any) => {
+    if (condition === value) {
+      self.pos = self.label[label];
+    }
+  };
+
+  for (let n = self.pos; n < self.code.length; n++) {
+    if (self.code[n] === DO_NOT_PROCESS) {
+      continue;
+    }
+    self.pos = n;
+    break;
+  }
+
+  let line = self.code[self.pos];
 
   try {
     eval(line);
